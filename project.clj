113aaef2fc199f86614e5c40d722779ad1392532
Gregg Reynolds <dev@mobileink.com>
2014-12-14 11:08:48 -0600

    proj file tweaks

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
   :test-selectors {:fields :fields
                    :meta :meta}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.9.14"]
-                 [log4j "1.2.17" :exclusions [javax.mail/mail
-                                              javax.jms/jms
-                                              com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 ;; [clj-logging-config "1.9.10"]
-                 [org.clojure/tools.logging "0.2.3"]])
+                 [com.google.appengine/appengine-api-1.0-sdk "1.9.17"]])
+                 ;; [log4j "1.2.17" :exclusions [javax.mail/mail
+                 ;;                              javax.jms/jms
+                 ;;                              com.sun.jdmk/jmxtools
+                 ;;                              com.sun.jmx/jmxri]]
+                 ;; [org.slf4j/slf4j-log4j12 "1.6.6"]
+                 ;; ;; [clj-logging-config "1.9.10"]
+                 ;; [org.clojure/tools.logging "0.2.3"]])
   ;; :profiles {:test {:dependencies [[com.google.appengine/appengine-api-stubs "1.8.4"]
   ;;                                  [com.google.appengine/appengine-testing "1.8.4"]
   ;;                                  [ring-zombie "1.0.1"]]}})
